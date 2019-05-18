bbc14a46db036fba0e41a2c39b1f6ad162aadca9
Jsbbvk <Jsbbvk@gmail.com>
2019-05-18 11:18:28 -0700

    init new files

--- js/main.js
+++ js/main.js
@@ -1,58 +1,62 @@
 // Initialize Firebase
 
-var imgArr = [];
 
-function getAllImages() {
+
+$("#refresh-button").on('mousedown touchstart', function(e){
+  e.preventDefault();
+  $(this).addClass("activeRefresh");
+})
+
+$("#refresh-button").on("mouseup touchend", function(e) {
+  e.preventDefault();
+  $(this).removeClass("activeRefresh");
+})
+
+var imgArr = [];
+var uid = "user1";
+function getAllImages(callback) {
     document.getElementById("displayImg").innerHTML = "";
-    db.collection("Users").doc("user1").collection("Images").get().then((col)=>{
+    db.collection("Users").doc(uid).collection("Images").get().then((col)=>{
+      var c = 0;
         col.forEach((img) => {
             new Promise(resolve => {
                 storage.ref().child("" + img.data().id).getDownloadURL().then(function (url) {
                     resolve(url);
                 });
             }).then((res) => {
+                c++;
                 imgArr.push({
                     name: img.data().name,
                     des: img.data().description,
                     id: img.data().id,
                     url: res
                 });
-                document.getElementById("displayImg").innerHTML += "<div class=\"img row\">" +
-                    "<div class=\"col-6 px-0\">" +
-                    "<img src=" + res + " class=\"img-fit\">" +
-                    "</div>" +
-                    "<div class=\"col-6\">" +
-                    "<h1>" + img.data().name + "</h1>" +
-                    "<h3>" + img.data().description + "</h3>" +
-                    "<a href=" + res + " target=\"_blank\">View in new window</a>" +
-                    "</div></div>"
+                if (c==col.size) {
+                  callback&&callback();
+                }
             });
         });
     });
 }
 
 function displayImages() {
-    var dis = document.getElementById("displayImg");
     new Promise(resolve=>{
-        var a = getAllImages();
-
-    }).then((images)=>{
+      imgArr = [];
+        getAllImages(()=>{resolve();});
+    }).then((res)=>{
         var str = "";
-        console.log(images);
-        images.forEach((img)=> {
-            console.log(img);
-            var url = img.url + "";
-            console.log(url);
-            str += "<div class=\"img row\">" +
-                "<div class=\"col-6 px-0\">" +
-                "<img src="+url+" class=\"img-fit\">" +
-                "</div>" +
-                "<div class=\"col-6\">" +
-                "<h1>"+img.name+"</h1>" +
-                "<h3>"+img.des+"</h3>" +
-                "<a href="+url+" target=\"_blank\">View in new window</a>" +
-                "</div></div>"
-        });
-        dis.innerHTML = str;
+        for (var img of imgArr) {
+          var url = img.url + "";
+          str += "<div class=\"img row\">" +
+              "<div class=\"col-6 px-0\">" +
+              "<img src="+url+" class=\"img-fit\">" +
+              "</div>" +
+              "<div class=\"col-6\">" +
+              "<h1>"+img.name+"</h1>" +
+              "<h3>"+img.des+"</h3>" +
+              "<a href="+url+" target=\"_blank\">View in new window</a>" +
+              "</div></div>";
+        }
+        document.getElementById("displayImg").innerHTML = str;
     });
 }
