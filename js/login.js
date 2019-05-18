5d2735a39c604784d4dcfbd330e2d264814ecbd6
sreeharirammohan@gmail.com <wizmathhelp@gmail.com>
2019-05-18 11:50:19 -0700

    Firebase auth

--- js/login.js
+++ js/login.js
@@ -4,7 +4,10 @@ $("#submit_form").on('click', function() {
   
   console.log(email + " " + password);
   
-  firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+  firebase.auth().signInWithEmailAndPassword(email, password).then(function(user) {
+    //user signed
+    
+  }).catch(function(error) {
     // Handle Errors here.
     var errorCode = error.code;
     var errorMessage = error.message;
@@ -13,3 +16,21 @@ $("#submit_form").on('click', function() {
 
 
 });
+
+$("#signup").onlick("click", function() {
+  email = $("#email").val();
+  password = $("#password").val();
+  
+  console.log(email + " " + password);
+  
+  firebase.auth().createUserWithEmailAndPassword(email, password).then(function(user) {
+    //created user successfully 
+    
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+  
+  });
+  
+})
